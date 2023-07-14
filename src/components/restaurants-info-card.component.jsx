20e09f41dbc92d483df01b753ecd619450232a1b
Meenaketan Padhy <meenaketanpadhy7@gmail.com>
2023-07-14 19:14:31 +0530

    complete card items.

--- src/components/restaurants-info-card.component.jsx
+++ src/components/restaurants-info-card.component.jsx
@@ -1,41 +1,69 @@
-import {Text as tx, View} from 'react-native';
+import {Text as tx, Image, View} from 'react-native';
 import React from 'react';
 import {Avatar, Button, Card, Text} from 'react-native-paper';
 import {styled} from 'styled-components';
+import {SvgXml} from 'react-native-svg';
+import star from '../assets/star';
+import open from '../assets/open';
+import {Spacer} from './spacer.componet';
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from './restaurant-info-card.styles';
 
 const RestaurantsInfoCard = ({restaurant = {}}) => {
   const {
     name = 'some restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
     ],
     address = '100 some random street',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{uri: photos[0]}} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text style={{color: 'red'}}>CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{uri: icon}} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
 
 export default RestaurantsInfoCard;
 
-const RestaurantCard = styled(Card)`
-  background-color: 'white';
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
-`;
-
 const Title = styled(Text)`
-  padding: 16px;
-  ${'' /* color: ${props => props.theme.colors.ui.primary}; */}
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${props => props.theme.fontSizes.body};
+  color: ${props => props.theme.colors.ui.primary};
 `;
